a4983a3cfc157ba94ca3c1b1abea66e39fdb5ac4
aaamam <aaamam@e2r3p2.1337.ma>
2024-01-23 16:11:03 +0100

    first commit

---
+++ includes/fract.h
@@ -0,0 +1,13 @@
+#ifndef FRACT_H
+# define FRACT_H
+
+# include <math.h>
+# include <mlx.h>
+# include <stdlib.h>
+
+# define WIDTH 800
+# define HEIGHT 800
+# define ITERATION 40.0
+# define ZOOM_FACTOR 1.1
+
+#endif
